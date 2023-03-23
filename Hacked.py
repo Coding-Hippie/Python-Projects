9627acacb1fe4663da105ccf3810fa22bd7ff221
Jacob Stack <97302068+Coding-Hippie@users.noreply.github.com>
2023-03-23 15:04:52 -0700

    Add files via upload

---
+++ Hacked.py
@@ -0,0 +1,31 @@
+# SPDX-FileCopyright Text: 2023 Jacob Stack
+# SPDX-License-Identifier: MIT
+
+# MACROPAD Hotkeys:
+
+from adafruit_hid.keycode import Keycode # REQUIRED if using Keycode.* values
+
+app = {# REQUIRED dict, must be named 'app'
+    'name' : 'Open AI', # Application name
+    'macros' : [# List of button macros...
+        # COLOR    LABEL    KEY SEQUENCE
+        # 1st row ----------
+        (0x002400, 'Win', ["""D00dle-420""", Keycode.ENTER]), # Text + Enter
+        (0x000090, 'TskMgr', [Keycode.CONTROL, Keycode.SHIFT, Keycode.ESCAPE]), # Task Manager
+        (0x002400, 'Excel', ["""resked2019""", Keycode.ENTER]),
+        # 2nd row ----------
+        (0x069690, 'PrtSc', [Keycode.PRINT_SCREEN]),
+        (0x000090, 'GUI', [Keycode.GUI, 'r']), # Opens windows run GUI console
+        (0x069420, 'Spotify', [Keycode.CONTROL, Keycode.KEYPAD_ONE]), # Runs programmed exe shortcut
+        # 3rd row ----------
+        (0x000090, 'Home', [Keycode.HOME]), # Moves cursor to beginning of line of text
+        (0x090049, ' /|\ ', [Keycode.CONTROL, Keycode.UP_ARROW]), # Moves the cursor up
+        (0x000090, 'ScrRcd', [Keycode.GUI, Keycode.ALT, 'r']), # Opens xbox screen recorder app
+        # 4th row ----------
+        (0x090049, '<<<', [Keycode.CONTROL, Keycode.LEFT_ARROW]), # Moves cursor to the next left space in any line of text
+        (0x071517, '\|/', [Keycode.CONTROL, 'q']),# Moves the cursor down
+        (0x073079, '>>>', [Keycode.CONTROL, Keycode.RIGHT_ARROW]),# Moves cursor to the next right space in any line of text
+        # Encoder button ---
+        (0x000000, '', [Keycode.CONTROL, Keycode.F4]) # Close window/tab
+    ]
+}
